e621de8dbd428e4afb568aba3af708369091bf80
dexterlab10 <dexterlab10@protonmail.com>
2019-05-08 12:58:38 -0300

    REX-41
    Optimizar migracion para actualizar totales de archivos de
    pagomiscuentas, ya que no se puede ejecutar por completo

--- migrations/m190429_214451_add_total_into_pagomiscuentas_table.php
+++ migrations/m190429_214451_add_total_into_pagomiscuentas_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use app\modules\pagomiscuentas\models\search\PagomiscuentasFileSearch;
 use yii\db\Migration;
 use app\modules\pagomiscuentas\models\PagomiscuentasFile;
+use yii\db\Query;
+use yii\db\Expression;
 
 /**
  * Class m190429_214451_add_total_into_pagomiscuentas_table
@@ -24,16 +25,26 @@ class m190429_214451_add_total_into_pagomiscuentas_table extends Migration
 
             if($pago_mis_cuentas_file->type == PagomiscuentasFile::TYPE_BILL) {
 
-                foreach ($pago_mis_cuentas_file->bills as $bill) {
-                    $total = $total + $bill['total'];
-                }
+                $total = (new Query())->select(new Expression('SUM(b.total) as total'))
+                    ->from('bill b')
+                    ->leftJoin('pagomiscuentas_file_has_bill phb', 'phb.bill_id = b.bill_id')
+                    ->where(['not',['phb.bill_id' => null]])
+                    ->andWhere(['phb.pagomiscuentas_file_id' => $pago_mis_cuentas_file->pagomiscuentas_file_id])
+                    ->all();
+
+                $total = $total[0]['total'];
             }
 
             if($pago_mis_cuentas_file->type == PagomiscuentasFile::TYPE_PAYMENT) {
 
-                foreach ($pago_mis_cuentas_file->payments as $payment) {
-                    $total = $total + $payment->amount;
-                }
+                $total = (new Query())->select(new Expression('SUM(p.amount) as total'))
+                    ->from('payment p')
+                    ->leftJoin('pagomiscuentas_file_has_payment php', 'php.payment_id = p.payment_id')
+                    ->where(['not',['php.payment_id' => null]])
+                    ->andWhere(['php.pagomiscuentas_file_id' => $pago_mis_cuentas_file->pagomiscuentas_file_id])
+                    ->all();
+
+                $total = $total[0]['total'];
             }
             $pago_mis_cuentas_file->updateAttributes(['total' => $total]);
         }
