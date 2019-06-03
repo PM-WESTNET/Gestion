089496d54a8a31689f1c586a2fbece5ff9d06d31
dexterlab10 <dexterlab10@protonmail.com>
2019-06-03 19:01:25 -0300

    Generacion de notas de credito a comprobantes duplicados

--- modules/sale/controllers/BatchInvoiceController.php
+++ modules/sale/controllers/BatchInvoiceController.php
@@ -9,15 +9,20 @@
 namespace app\modules\sale\controllers;
 
 
+use app\modules\config\models\Config;
+use app\modules\sale\components\BillExpert;
 use app\modules\sale\models\BillType;
 use app\modules\sale\models\Company;
 use app\modules\sale\models\Customer;
+use app\modules\sale\models\PointOfSale;
 use app\modules\sale\models\search\BillSearch;
+use app\modules\sale\models\TaxCondition;
 use app\modules\sale\modules\contract\components\ContractToInvoice;
 use app\modules\sale\modules\contract\models\Contract;
 use app\modules\sale\modules\contract\models\search\ContractSearch;
 use Yii;
 use yii\data\ActiveDataProvider;
+use yii\db\Query;
 use yii\helpers\Json;
 use app\components\web\Controller;
 use yii\widgets\ActiveForm;
@@ -199,6 +204,107 @@ class BatchInvoiceController  extends Controller
                 'messages' => $retMessages
             ];
         }
+    }
+
+    /**
+     * @param $company_id
+     * @param $date
+     * @param null $limit
+     * @return bool
+     * @throws \yii\base\InvalidConfigException
+     *
+     */
+    public function actionFixDoubleBills($company_id, $date, $limit = null)
+    {
+        set_time_limit(0);
+        $bills_not_closed = '';
+        $bills_closed = '';
+
+        $customersDuplicatedBillsQuery= (new Query())
+            ->select(['customer_id'])
+            ->from('bill')
+            ->andWhere(['date' => \Yii::$app->formatter->asDate($date, 'yyyy-MM-dd')])
+            ->andWhere(['<>','total', 0])
+            ->groupBy(['customer_id'])
+            ->having(['>', 'count(*)', 1])
+            ->limit($limit);
+
+        $customersDuplicatedBills = $customersDuplicatedBillsQuery->all();
+        $customers_id = array_map(function($customer){ return $customer['customer_id'];}, $customersDuplicatedBills);
+
+        $customers = Customer::find()
+            ->andWhere(['IN', 'customer_id', $customers_id])
+            ->andWhere(['company_id' => $company_id])
+            ->all();
+
+        $point_of_sale = PointOfSale::findOne(['company_id' => $company_id, 'default' => 1]);
+
+        if (empty($point_of_sale)) {
+            return false;
+        }
+
+        foreach ($customers as $customer) {
+            $taxIvaInscr = TaxCondition::findOne(['name' => 'IVA Inscripto']);
+
+            if ($customer->tax_condition_id === $taxIvaInscr->tax_condition_id) {
+                $bill_type = BillType::findOne(['name' => 'Nota Crédito A']);
+            }else {
+                $bill_type = BillType::findOne(['name' => 'Nota Crédito B']);
+            }
+
+            $bills = $customer->getBills()->orderBy(['bill.timestamp' => SORT_DESC])->all();
+
+            if (!empty($bills) && $bills[0]->total === $bills[1]->total && $bills[0]->bill_type_id == $bills[1]->bill_type_id) {
+                $bill = $this->createBill($bill_type->bill_type_id, $point_of_sale->point_of_sale_id,$customer, $bills[0]->amount, $bills[0]->total);
+                if($bill->close()){
+                    $bills_closed .= $bill->bill_id .', ';
+                } else {
+                    $bills_not_closed .= $bill->bill_id .', ';
+                };
+            }
+        }
+
+        if($bills_not_closed) {
+            Yii::$app->session->setFlash('error', 'Cant close bills:' .$bills_not_closed);
+        }
+
+        if($bills_closed) {
+            Yii::$app->session->setFlash('success', 'Closed bills:' .$bills_closed);
+        }
+
+        if(empty($bills_closed) && empty($bills_not_closed)) {
+            Yii::$app->session->setFlash('info', 'No hay comprobantes duplicados para generar notas de credito');
+        }
+
+        return $this->redirect(['/sale/bill']);
+    }
+
+    public function createBill($bill_type_id, $point_of_sale_id, $customer, $net, $total)
+    {
+        $default_unit_id = Config::getValue('default_unit_id');
+        $bill = BillExpert::createBill($bill_type_id);
+        $bill->company_id = $customer->company->company_id;
+        $bill->point_of_sale_id = $point_of_sale_id;
+        $bill->customer_id = $customer->customer_id;
+        $bill->status = 'draft';
+
+        $bill->save(false);
+
+        $a = $bill->addDetail([
+            'product_id' => null,
+            'unit_id' => $default_unit_id,
+            'qty' => 1,
+            'type' => null,
+            'unit_net_price' => abs($net),
+            'unit_final_price' => abs($total),
+            'concept' => 'Corrección error de Facturación',
+            'discount_id' => null,
+            'unit_net_discount' => null,
+        ]);
+
+        $bill->fillNumber();
+
+        return $bill;
     }
 
 }
