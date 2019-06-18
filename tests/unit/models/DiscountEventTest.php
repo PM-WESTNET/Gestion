e44321d4ed06d86dccca244ffda447d59820624f
dexterlab10 <dexterlab10@protonmail.com>
2019-06-18 14:53:35 -0300

    DiscountEventTest

---
+++ tests/unit/models/DiscountEventTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dexterlab10
+ * Date: 18/06/19
+ * Time: 14:51
+ */
+use app\modules\sale\models\DiscountEvent;
+use app\tests\fixtures\ProductFixture;
+
+class DiscountEventTest extends \Codeception\Test\Unit
+{
+
+    public function _fixtures()
+    {
+        return [
+            'product' => [
+                'class' => ProductFixture::class
+            ],
+        ];
+    }
+
+
+    public function testValidWhenFullAndNew()
+    {
+        $model = new DiscountEvent([
+        ]);
+
+        expect('Valid when full and new', $model->validate())->true();
+    }
+
+    public function testSaveWhenFullAndNew()
+    {
+        $model = new DiscountEvent([
+
+        ]);
+
+        expect('Valid when full and new', $model->save())->true();
+    }
+}
