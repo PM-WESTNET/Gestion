95ab6012f3b56bb77bdea47eb49158e6b1f51abd
Goku <gohangoku@protonmail.com>
2019-07-11 13:16:24 -0300

    Init ivr module

---
+++ modules/ivr/IvtModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\ivr;
+
+/**
+ * ivr module definition class
+ */
+class IvtModule extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\ivr\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
