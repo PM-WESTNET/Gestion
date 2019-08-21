308fc438c60eb60cfac00aee2bfbed411be81fa7
Goku <gohangoku@protonmail.com>
2019-08-21 18:11:39 -0300

    Fix last migration

--- migrations/m190821_160334_instalation_schedule_default_value.php
+++ migrations/m190821_160334_instalation_schedule_default_value.php
@@ -12,7 +12,7 @@ class m190821_160334_instalation_schedule_default_value extends Migration
      */
     public function safeUp()
     {
-        $this->alterColumn('contract', 'instalation_schedule', 'enum("in the morning", "in the afternoon", "all day") NOT NULL DEFAULT "all day"');
+        $this->alterColumn('contract', 'instalation_schedule', 'enum("in the morning", "in the afternoon", "all day") NULL DEFAULT "all day"');
     }
 
     /**
