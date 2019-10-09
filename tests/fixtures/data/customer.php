406590755a5fac1d57d45da5d143f9462ff5eef2
Goku <gohangoku@protonmail.com>
2019-10-09 17:43:28 -0300

    Tests para reglas de negocios de IVR

--- tests/fixtures/data/customer.php
+++ tests/fixtures/data/customer.php
@@ -37,6 +37,15 @@ $customer = [
         'sms_fields_notifications' => 'phone,phone2,phone3','email_fields_notifications' => 'email,email2',
         'needs_bill' => '0','parent_company_id' => 8, 'payment_code' => 999000111225, 'current_account_balance' => 1500,
         'last_balance' => 1563408000],
+    ['customer_id' => 45904,'name' => 'Maria','lastname' => 'GOMEZ','document_number' => '35875229',
+        'sex' => NULL,'email' => 'jperez7@quoma.com.ar','phone' => '2612575620','status' => 'enabled',
+        'document_type_id' => 2,'tax_condition_id' => 3,'account_id' => NULL,'company_id' => NULL,
+        'address_id' => 1,'email2' => 'jperez8@quoma.com.ar','phone2' => '2612575620','phone3' => '2612575620',
+        'customer_reference_id' => NULL,'code' => '59813','payment_code' => NULL,'publicity_shape' => 'poster',
+        'screen_notification' => '1','sms_notification' => '1','email_notification' => '1',
+        'sms_fields_notifications' => 'phone,phone2,phone3','email_fields_notifications' => 'email,email2',
+        'needs_bill' => '0','parent_company_id' => 8, 'payment_code' => 999000111225, 'current_account_balance' => 1500,
+        'last_balance' => 1563408000],
 ];
 
 return $customer;
