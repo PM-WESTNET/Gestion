6a926b44815f7071bb7bf557561109e0d968e7d0
Goku <gohangoku@protonmail.com>
2019-11-07 12:45:19 -0300

    Se devuelve un solo contrato al IVR a pedido de Iperfex

--- modules/ivr/v1/models/Customer.php
+++ modules/ivr/v1/models/Customer.php
@@ -35,14 +35,16 @@ class Customer extends \app\modules\sale\models\Customer
                 'phone3',
                 'phone4',
                 'contracts' => function($model) {
-                    $contracts = [];
-                    foreach ($model->contracts as $contract) {
-                        if($contract->status === Contract::STATUS_ACTIVE){
-                            $contracts[] = [
-                                'contract_id' => $contract->contract_id,
-                                'service_address' => $contract->address ? $contract->address->fullAddress : $this->address,
-                            ];
-                        }
+                    $contracts = [
+                        'contract_id' => '',
+                        'service_address' => ''
+                    ];
+                    $contract = $this->getContracts()->andWhere(['status' => Contract::STATUS_ACTIVE])->one();
+                    if($contract){
+                        $contracts= [
+                            'contract_id' => $contract->contract_id,
+                            'service_address' => $contract->address ? $contract->address->fullAddress : $this->address,
+                        ];
                     }
 
                     return $contracts;
@@ -99,11 +101,15 @@ class Customer extends \app\modules\sale\models\Customer
     {
         $payment_extension_product = Product::findOne(Config::getValue('extend_payment_product_id'));
 
-        $contracts = [];
+        $contracts = [
+            'contract_id' => '',
+            'service_address' => '',
+        ];
         $payment_extension_requested = $this->getPaymentExtensionQtyRequest();
+        $contract = $this->getContracts()->andWhere(['status' => Contract::STATUS_ACTIVE])->one();
 
-        foreach ($this->contracts as $contract) {
-            $contracts[] = [
+        if ($contract) {
+            $contracts = [
                 'contract_id' => $contract->contract_id,
                 'service_address' => $contract->address ? $contract->address->fullAddress : $this->address,
             ];
