1ca48c473d0c8f53bbefe0664499d07ee7dd9c22
Juan Pablo Doña <juanpablodona@gmail.com>
2020-05-04 15:16:27 -0300

    Modificacion de directorios remotos de destino de backups

--- commands/BackupMysqlController.php
+++ commands/BackupMysqlController.php
@@ -91,7 +91,7 @@ class BackupMysqlController extends \yii\console\Controller
         if ($result ==  '' && file_exists($fileOut)) {
             try {
 
-                if($this->transferToRemoteServer($name, '/mnt/pruebaRaid/backupGestion/percona/full/'. $name)) {
+                if($this->transferToRemoteServer($name, '/mnt/pruebaRaid/backupGestion/percona/full/'. $date->format('Y-m-d_H-i'). '.tar')) {
                     $backup->status = 'success';
                 }else {
                     $backup->description = 'Backup Realizado localmente. No se pudo transferir a servidor de backups';
