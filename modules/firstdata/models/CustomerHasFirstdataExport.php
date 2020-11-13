853b4ec6e67df617cec1d976707188c7d1e15141
Juan Pablo Doña <juanpablodona@gmail.com>
2020-11-13 18:27:46 -0300

    Customer has firstdata export

---
+++ modules/firstdata/models/CustomerHasFirstdataExport.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\modules\firstdata\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "customer_has_firstdata_export".
+ *
+ * @property int $customer_has_firstdata_export_id
+ * @property int $customer_id
+ * @property int $firstdata_export_id
+ * @property string $month
+ *
+ * @property Customer $customer
+ * @property FirstdataExport $firstdataExport
+ */
+class CustomerHasFirstdataExport extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'customer_has_firstdata_export';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['customer_id', 'firstdata_export_id'], 'integer'],
+            [['month'], 'string', 'max' => 255],
+            [['customer_id'], 'exist', 'skipOnError' => true, 'targetClass' => Customer::className(), 'targetAttribute' => ['customer_id' => 'customer_id']],
+            [['firstdata_export_id'], 'exist', 'skipOnError' => true, 'targetClass' => FirstdataExport::className(), 'targetAttribute' => ['firstdata_export_id' => 'firstdata_export_id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'customer_has_firstdata_export_id' => Yii::t('app', 'Customer Has Firstdata Export ID'),
+            'customer_id' => Yii::t('app', 'Customer ID'),
+            'firstdata_export_id' => Yii::t('app', 'Firstdata Export ID'),
+            'month' => Yii::t('app', 'Month'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCustomer()
+    {
+        return $this->hasOne(Customer::className(), ['customer_id' => 'customer_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFirstdataExport()
+    {
+        return $this->hasOne(FirstdataExport::className(), ['firstdata_export_id' => 'firstdata_export_id']);
+    }
+}
