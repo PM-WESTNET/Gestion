a642d639b038a0fcde32c55e8e67626c034bfa27
Gabriel M. Bermudez <gbermudez@westnet.com.ar>
2021-04-23 16:24:31 -0300

    Agrego archivo init para inicializar proyectos para westnet o bigway, cada uno con su correspondiente carpeta docker para levantar cotenedores, ademas se agrega un docker_sample para tener uno configurado de manera incial, y se modifican .htaccess para acceder a la carpeta web/index.php

---
+++ environments/bigway/config/db.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    'db' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_westnet',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbafip' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_afip',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbconfig' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_config',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbagenda' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_agenda',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbticket' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_ticket',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbecopago' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_ecopago',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbnotifications' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_notifications',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dblog' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_log',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbmedia' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=gestion_media',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+    'dbbackups' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=gestion-bg-data;dbname=rex-backups',
+        'username' => 'root',
+        'password' => 'secret',
+        'charset' => 'utf8',
+    ],
+];
