8ada8671cd51ef30c849617e22ba48f72507e044
Gabriel M. Bermudez <gbermudez@westnet.com.ar>
2021-05-20 12:16:13 -0300

    AD-285 ADD: Se agrega la opcion de elegir la empresa al importar el txt para firstdata

--- modules/firstdata/views/firstdata-import/create.php
+++ modules/firstdata/views/firstdata-import/create.php
@@ -16,7 +16,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $this->render('_form', [
         'model' => $model,
-        'accounts' => $accounts
+        'accounts' => $accounts,
+        'companies_config' => $companies_config,
     ]) ?>
 
 </div>
--- modules/firstdata/views/firstdata-import/update.php
+++ modules/firstdata/views/firstdata-import/update.php
@@ -18,6 +18,7 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 
     <?= $this->render('_form', [
         'model' => $model,
+        'companies_config' => $companies_config,
     ]) ?>
 
 </div>
