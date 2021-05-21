4433aabe54d4c263990822cbe5374d5f34e0d80c
Gabriel M. Bermudez <gbermudez@westnet.com.ar>
2021-05-21 15:16:39 -0300

    AD-161 ADD: Agrego primera version del reporte solicitado

--- modules/westnet/reports/controllers/ReportsController.php
+++ modules/westnet/reports/controllers/ReportsController.php
@@ -16,10 +16,6 @@ use app\components\web\Controller;
 
 use app\components\helpers\GraphData;
 
-use app\components\helpers\ExcelExporter;
-
-use PHPExcel_Style_NumberFormat;
-
 use app\modules\config\models\Config;
 
 use app\modules\sale\models\Customer;
@@ -37,7 +33,6 @@ use app\modules\westnet\models\search\NotifyPaymentSearch;
 use app\modules\westnet\models\search\PaymentExtensionHistorySearch;
 use app\modules\westnet\models\search\ConnectionForcedHistorialSearch;
 
-use webvimark\modules\UserManagement\models\User;
 
 use app\modules\checkout\models\PaymentMethod;
 
