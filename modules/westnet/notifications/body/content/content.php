baed30b2fa1c392cb8330fd0beb905020ab7494c
emilasheras <emperorbfd@gmail.com>
2021-07-07 09:38:30 -0300

    fix del check

--- modules/westnet/notifications/body/content/content.php
+++ modules/westnet/notifications/body/content/content.php
@@ -10,8 +10,9 @@ if(isset($notification)) {
     $notification = Yii::$app->view->params['notification'];
 }
 
-// check que no se rompa la app si el contenido de la notificacion esta vacio (ya habiendo apretado el boton de actualizar)
-if (isset($notification->content)){
+// check que no se rompa la app si el contenido de la notificacion esta vacio 
+//(ya habiendo apretado el boton de actualizar)
+if (!isset($notification->content)){
     $notification->content = "!!cuerpo de notificacion vacio!!";
     Yii::warning("Actualizar notificacion - cuerpo vacio");
 }
