60df37d1d4c59689653d3a9aa0f6c24b28873b48
root <root@Gestion-test.westnet.com.ar>
2021-07-30 18:14:22 +0000

    Quito conflicto de merge

--- modules/westnet/notifications/controllers/NotificationController.php
+++ modules/westnet/notifications/controllers/NotificationController.php
@@ -573,13 +573,7 @@ class NotificationController extends Controller {
         $paymentIntention->fecha_operacion = $result_search['FechaOperacion'];
         $paymentIntention->fecha_registro = $result_search['FechaRegistro'];
         $paymentIntention->save(false);
-<<<<<<< HEAD
-        
-        
-=======
-       
 
->>>>>>> origin/BDP-23-intencion-de-pago-cancelada
         if($result_search['PagoExitoso'] == 'payed'){
             $transaction = Yii::$app->db->beginTransaction();
             $customer = Customer::findOne(['customer_id' => $paymentIntention->customer_id]);
