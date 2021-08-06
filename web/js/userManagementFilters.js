59d0664966bdf019264a85d4e6f8afe84383268d
emilasheras <emperorbfd@gmail.com>
2021-08-06 12:29:50 -0300

    fix

--- web/js/userManagementFilters.js
+++ web/js/userManagementFilters.js
@@ -1,63 +1,64 @@
 $(document).ready(function () {
-    //get body id tag (that is setted by PHP in the server side)
     const bodyID = $('body').attr('id');
-    //console.log(bodyID + "~ type: " + typeof bodyID);
 
-    //array with all views to apply filter to
     const specificViewFilters = [
         "user-management-permission-view",
         "user-management-role-view",
         "user-management-user-permission-set",
     ];
-    //console.log(specificViewFilters);
-
-    //return true if the current view has the id of a value specified in the previous array
-    /* specificViewFilters.some((i) => {
-      console.log(`value ${i} is ` + (i === bodyID));
-      return i === bodyID;
-    }); */
 
     if (specificViewFilters.some((i) => {
-        console.log(`value ${i} is ` + (i === bodyID));
+        //console.log(`value ${i} is ` + (i === bodyID));
         return i === bodyID;
     })) {
-        console.log("valid view ~ script running");
+        //console.log("valid view ~ script running");
         var firstForm = $(".panel-body form").first();
         firstForm.prepend(
             '<div class="row" style="overflow: hidden;"><div class="col-sm-12"><input id="search-in-child-permissions" autofocus="off" type="text" class="form-control input-sm" placeholder="buscar"></div></div><hr/>'
         );
 
         $("#search-in-child-permissions").on("change keyup", function () {
-            //console.log("key pressed");
             var input = $(this);
-            //console.log(input.val());
-
-            if (input.val() == "") {
-                firstForm.find("label").each(function () {
-                    $(this).removeClass("hide");
-                });
-                return;
-            }
-            //console.log(firstForm.html());
 
             firstForm.find("label").each(function () {
                 var _t = $(this);
-                //console.log(_t);
                 var labelText = _t[0].innerText;
-                //console.log("::::::"+_t.html().indexOf(input.val()));
 
                 if (_t.html().indexOf(input.val()) > -1) {
-                    _t.closest("label").removeClass("hide");
-                    _t.closest("label").next().removeClass("hide");
-                    _t.closest("label").next().next().removeClass("hide");
+                    //console.log(input.val());
+                    _t.closest("label").show();
+                    _t.closest("label").next().show();
+                    _t.closest("label").next().next().show();
                 } else {
-                    _t.closest("label").addClass("hide");
-                    _t.closest("label").next().addClass("hide");
-                    _t.closest("label").next().next().addClass("hide");
+                    _t.closest("label").hide();
+                    _t.closest("label").next().hide();
+                    _t.closest("label").next().next().hide();
                 }
             });
         });
     } else {
-        console.log("body ID doesnt match");
+        //console.log("body ID doesnt match");
     }
 });
+/* 
+ $(document).ready(function () {
+    //console.log('ready ~ script runns');
+    $("#search").on("keyup", function () {
+        if (this.value.length > 0) {
+            $("li", ".navbar-nav").not(".not-filter")
+                .hide()
+                .filter(function () {
+                    return (
+                        $(this)
+                            .text()
+                            .toLowerCase()
+                            .indexOf($("#search").val().toLowerCase()) != -1
+                    );
+                })
+                .show();
+        } else {
+            $("li", ".navbar-nav").not(".not-filter").show();
+        }
+    });
+});
+  */
