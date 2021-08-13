26f4ffde13a406e08af0f85a23582006130acaf8
emilasheras <emperorbfd@gmail.com>
2021-08-13 14:04:06 -0300

    bugfixes

--- web/js/searchFilter.js
+++ web/js/searchFilter.js
@@ -1,15 +1,17 @@
-$(document).ready(function () {
+$(document).ready(function (evt) {
     var searchBar = $("#search");
-
     searchBar.on("keyup", function (event) {
         var inputValue = this.value;
-        var srcStrHtml = $("ul#w0").find("li");
-
+        //var srcStrHtml = $("ul#menu-layout-navbar").find("li:has(a[href^='/']),li.divider,li.dropdown-header");
+        var srcStrHtml = $("ul#menu-layout-navbar").children().add($("ul#menu-layout-navbar").find("a[href^='/'],li"));
+        
+        //$("li").child("a")
         if (inputValue.length > 0) {
             
             srcStrHtml.hide();
             var srcStrHtml = srcStrHtml.filter(function () {
-                
+                //$(this).children(".li[style*='display: none']").each(function () {console.log($(this))});
+                //if($(this).is("li")){console.log($(this))};
                 var text = $(this).text();
                 text = removeAccents(text);
                 text = text.toLowerCase();
@@ -19,12 +21,14 @@ $(document).ready(function () {
                 );
             });
             srcStrHtml.show();
-            
+            /* srcStrHtml.each(function(){
+                $(this).addClass("highlight");
+            }); */
         } else {
             srcStrHtml.show();
-            srcStrHtml.each(function(){
+            /* srcStrHtml.each(function(){
                 $(this).removeClass("highlight");
-            });
+            }); */
         }
     });
 
@@ -38,23 +42,26 @@ $(document).ready(function () {
     // CSS logic
     const icon = $('.search-icon');
     const search = $('.search-container');
-    var locked = false;
     const animationTime = 300;
     icon.click(function (evt) {
         evt.stopImmediatePropagation();
+        animateSearchBar();
+    });
+
+    var locked = false;
+    const animateSearchBar = ()=>{
         if(!locked){
             locked = true;
             setTimeout(unlock, animationTime);
             // we utilize this function to prevent a propagation error. https://stackoverflow.com/questions/8238599/jquery-click-fires-twice-when-clicking-on-label/49336534
             search.toggleClass('active');
-            (search.hasClass('active')) ? setTimeout(() => {$("#search").focus();}, animationTime) : clearSearchBar();        
+            (search.hasClass('active')) ? setTimeout(() => {focusSearchBar();}, animationTime) : clearSearchBar();        
         }
-        
-    });
+    }
+
     function unlock() {
         locked = false;
     }
-
 
     // clear button logic
     const clearBtn = $('.close-icon').children('span');
@@ -62,6 +69,13 @@ $(document).ready(function () {
 
     const clearSearchBar = ()=>{
         searchBar.val('');
+        if(search.hasClass('active'))focusSearchBar();
         searchBar.keyup();
     }
+    const focusSearchBar = ()=>{
+        $("#search").focus();
+    }
+
+    animateSearchBar();
+    console.log('Search-bar initialized.');
 });
