3c005c79ad138729e594233bfcd7d26032d8299f
emilasheras <emperorbfd@gmail.com>
2021-08-18 14:58:12 -0300

    first ver of mail layouts

--- modules/westnet/notifications/body/layouts/PaymentButton.php
+++ modules/westnet/notifications/body/layouts/PaymentButton.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 use app\modules\config\models\Config;
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -12,45 +13,35 @@ $title = $notification['subject'];
 <?php $this->beginPage() ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $title ?></title>
-    <?php $this->head() ?>
-  </head>
-  <body>
-    <div class="content" style="background-color: white; margin: 0 auto; color: #292929; width: 600px;">
-      <table style="width: 100%; font-family: Arial, Helvetica, sans-serif;">
-        <thead>
-          <tr>
-            <th colspan="9">
-            <?= Html::img(Url::base(true).'/images/logo-westnet.png', ['alt' => 'Westnet', 'style' => 'margin: 0 auto; width: 140px;']) ?>
-            </th>
-          </tr>
-        </thead>
 
-        <tbody>
-          <!-- Mensaje Danger -->
-          <tr>
-            <td style="background-color: red; padding: 10px; color: white;">
-               <h4 style="margin: 10px; text-align: center; line-height: 1.5em;"><?= Config::getValue('mail-top-title') ?></h4>
-               <h2 style="margin: 10px; text-align: center; line-height: 1.5em;"><?= $title ?></h2>
-            </td>            
-          </tr>
-          
-          <tr>
-            <td>
-                <?= $notification['content'] ?>
-            </td>
-          </tr>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $title ?></title>
+  <?php $this->head() ?>
+</head>
 
-        </tbody>
+<body>
+  <div class="content" style="background-color:#EEEEEE;margin:0 auto;color:#292929;width:100%;">
+  <table align="center" cellpadding="0" cellspacing="0" width="100%" height="10%" style="background:red;">
+      <tr style="background:#1C3AE2;border-radius:15px;width:640px;">
+        <td style="background:#3D3D3D;font-size:0;border-radius:25px 0 0 6px;height:100%;width:40px;">&nbsp;</td>
+        <td >&nbsp;</td>
+        <td style="border-radius:0 15px 0 0; height:100%; width:4px;">&nbsp;</td>
+      </tr>  
+    </table>
+    <table align="center" cellpadding="0" cellspacing="0" width="100%" height="100" style="background:red;">
 
-        <?= $this->renderFile('@app/modules/westnet/notifications/body/partials/_footer.php', ['company'=> $notification['emailTransport']->getObject()]) ?>
-        
-      </table>
-    </div>
-  </body>
+      <tr style="background:#1C3AE2;">
+        <td style="border-radius:15px 0 0 0; height:100%;">&nbsp;</td>
+        <td style="background:#FFFFFF;width:70%;">&nbsp;</td>
+        <td style="border-radius:0 15px 0 0; height:100%;">&nbsp;</td>
+      </tr>      
+    </table>
+  </div>
+</body>
+
 </html>
 <?php $this->endPage() ?>
+
