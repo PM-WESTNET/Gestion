f0abeb27378b6e69c774e3da492ee137197e87fe
emilasheras <emperorbfd@gmail.com>
2021-08-20 09:24:53 -0300

    avances hasta la fecha

--- modules/westnet/notifications/body/layouts/PaymentButton.php
+++ modules/westnet/notifications/body/layouts/PaymentButton.php
@@ -19,29 +19,88 @@ $title = $notification['subject'];
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= $title ?></title>
+  <style type="text/css">
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    table {
+      border-spacing: 0;
+    }
+    td{
+      padding:0;
+    }
+    img{
+      border:0;
+    }
+
+
+    @media screen and (max-width:600px){
+
+    }
+
+    @media screen and (max-width:400px){
+
+    }
+
+  </style>
   <?php $this->head() ?>
+  
 </head>
 
 <body>
-  <div class="content" style="background-color:#EEEEEE;margin:0 auto;color:#292929;width:100%;">
-  <table align="center" cellpadding="0" cellspacing="0" width="100%" height="10%" style="background:red;">
-      <tr style="background:#1C3AE2;border-radius:15px;width:640px;">
-        <td style="background:#3D3D3D;font-size:0;border-radius:25px 0 0 6px;height:100%;width:40px;">&nbsp;</td>
-        <td >&nbsp;</td>
-        <td style="border-radius:0 15px 0 0; height:100%; width:4px;">&nbsp;</td>
-      </tr>  
-    </table>
-    <table align="center" cellpadding="0" cellspacing="0" width="100%" height="100" style="background:red;">
-
+  <div class="content" style="background-color:#f9f9f9;margin:0 auto;color:#292929;width:100%;">
+    <table align="center" cellpadding="0" cellspacing="0" width="100%" style="background:#f9f9f9;">
       <tr style="background:#1C3AE2;">
-        <td style="border-radius:15px 0 0 0; height:100%;">&nbsp;</td>
-        <td style="background:#FFFFFF;width:70%;">&nbsp;</td>
-        <td style="border-radius:0 15px 0 0; height:100%;">&nbsp;</td>
-      </tr>      
+        <td style="background:#1C3AE2;width:2rem;">&nbsp;</td>
+        <td style="height:4rem;">&nbsp;</td>
+        <td style="background:#1C3AE2;width:2rem;">&nbsp;</td>
+      </tr>
+      <tr style="background:#1C3AE2;">
+        <td style="background:#1C3AE2;width:available;">&nbsp;</td>
+        <td style="height:100%;">
+          <table align="center" cellpadding="0" cellspacing="0" width="500px" style="background:#f9f9f9;">
+            <tr>
+              <td style="height:2rem;width:auto;">&nbsp;</td>
+            </tr>
+            <tr >
+              <td style="width:2rem;">&nbsp;</td>
+              <td style="width:auto;">
+                <table align="center" cellpadding="0" cellspacing="0" width="100%" style="background:#FFFFFF;">
+                  <tr>
+                    <td>
+                      <table align="center" cellpadding="0" cellspacing="0" width="100%" style="background:red;">
+                        <tr><td style="height:5px;">&nbsp;</td></tr>
+                        <tr><td style="text-align:center;font-size:25px;font-weight:bold;line-height:1.2;vertical-align:bottom">
+                      Su factura ya se encuentra disponible
+                    </td></tr>
+                    <tr><td>&nbsp;</td></tr>
+                      </table>
+                    </td>
+                    
+                  </tr>
+                  
+                </table>
+              </td>
+              <td style="width: 2rem;">&nbsp;</td>
+            </tr>
+          </table>
+        </td>
+        <td style="background:#1C3AE2;width:available;">&nbsp;</td>
+        
+      </tr>
+      <tr style="background:#1C3AE2;">
+        <td style="background:#1C3AE2;width:available;">&nbsp;</td>
+        <td style="height:100%;">
+        &nbsp;
+        </td>
+        <td style="background:#1C3AE2;width:available;">&nbsp;</td>
+        
+      </tr>
     </table>
+
   </div>
 </body>
 
 </html>
 <?php $this->endPage() ?>
-
