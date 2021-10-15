67af9ac1ef4de6cfe024ed590387ba79710e699d
emilasheras <emperorbfd@gmail.com>
2021-10-15 11:06:07 -0300

    fix definitivo creacion de ADS con multiples nodos

--- modules/westnet/views/ads/empty-ads.php
+++ modules/westnet/views/ads/empty-ads.php
@@ -63,7 +63,7 @@ $model = new Node();
                 $(document).off('click', '#generate').on('click', '#generate', function (e) {
                     e.preventDefault();
                     var cantidad = $("#node-count").val();
-                    var node = $("#node-node_id").val().length;
+                    var node = $("#node-node_id").val();
                     var company = $("#company_id").val();
 
                     if(!company){
@@ -77,7 +77,7 @@ $model = new Node();
                     }
 
 
-                    if( node == 0){
+                    if( node.length == 0){
                         alert('<?php echo Yii::t('westnet','The node can\'t be empty.')  ?>');
                         return;
                     }
