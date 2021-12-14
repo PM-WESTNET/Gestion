58fc7b114b54a5cad73de06d0f5310b5632f1efe
emilasheras <emperorbfd@gmail.com>
2021-12-14 15:55:13 -0300

    progress to date. added comments and trying to solve the mutex problem. still in progress.

--- commands/VendorLiquidationController.php
+++ commands/VendorLiquidationController.php
@@ -16,26 +16,38 @@ use app\modules\checkout\models\search\PaymentSearch;
 class VendorLiquidationController extends Controller{
 
     public function actionLiquidationByLot(){
-        // todo: implement MUTEX library for this controller. So as to run only if no other liquidation is running ...
-
         $this->stdout("\nLiquidation By Lot initiated.\n"); // you can catch this output by using >> as an extra parameter via command line interface
         
         $model = VendorLiquidationProcess::find()->where(['status' => 'pending'])->one();
-        $this->LiquidationProcess($model);
 
-        $model->status = 'success';
-        // todo: dont force the saving. so as not to break anything in the future that might rely on 'perfect' data structures
-        $model->save(false);
-        /* if($model->save()){
-            do ...
-        } else {
-            error catch and summary() ...
-        } */
-        
-        $vendor_liquidations = VendorLiquidation::find()->where(['vendor_liquidation_process_id' => $model->vendor_liquidation_process_id])->all();
+        if($model) { // if liquidation ANY 'pending' vendor liquidation is found ...
+            try {
+                if(Yii::$app->mutex->acquire('mutex_liquidate_vendors')) { // only one liquidation can be active at a time
+                    $this->stdout("Vendor Liquidation Process ID: ".$model->vendor_liquidation_process_id."\n");
 
-        foreach ($vendor_liquidations as $key => $value) {
-	        if($value->status == "cancelled" || empty($value->total)) $value->delete();
+                    $this->LiquidationProcess($model);
+                    $model->status = 'success';
+                    
+                    // todo: dont force the saving. so as not to break anything in the future that might rely on 'perfect' data structures
+                    $model->save(false);
+
+                    /* if($model->save()){
+                        do ...
+                    } else {
+                        error catch and summary() ...
+                    } */
+                    
+                    $vendor_liquidations = VendorLiquidation::find()->where(['vendor_liquidation_process_id' => $model->vendor_liquidation_process_id])->all();
+            
+                    foreach ($vendor_liquidations as $key => $value) {
+                        if($value->status == "cancelled" || empty($value->total)) $value->delete();
+                    }
+                }
+            } catch (\Exception $ex) {
+                $err_msg="ERROR__________". 'Linea '.$ex->getLine()."\n" .'Archivo '.$ex->getFile() ."\n" .$ex->getMessage() ."\n" .$ex->getTraceAsString()."\n";
+                $this->stdout($err_msg);
+                //\Yii::info('ERROR ________________ ' . $ex->getMessage() ."\n" .$ex->getTraceAsString(), 'facturacion-creacion');
+            }
         }
     }
 
--- modules/westnet/views/vendor-liquidation/vendor-liquidation-process.php
+++ modules/westnet/views/vendor-liquidation/vendor-liquidation-process.php
@@ -5,6 +5,7 @@ use yii\widgets\DetailView;
 use yii\grid\GridView;
 use kartik\widgets\Select2;
 use app\components\helpers\UserA;
+use yii\helpers\Url;
 
 $this->title = Yii::t('westnet', 'Proceso de Liquidación de Vendedores');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('westnet', 'Vendor Liquidations Process'), 'url' => ['index']];
@@ -15,10 +16,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p class="pull-right">
-        <?= UserA::a("<span class='glyphicon glyphicon-plus'></span> " . 'Calcular Comisiones (Nuevo Proceso)', 
-        ['batch-vendor-liquidation-process'], 
-        ['class' => 'btn btn-success']) 
-        ;?>
+        <?= UserA::a(
+            "<span class='glyphicon glyphicon-plus'></span> " . 'Nuevo Proceso (Calculo de Comisiones)',
+            ['batch-vendor-liquidation-process'],
+            ['class' => 'btn btn-success']
+        ); ?>
     </p>
 
 
@@ -36,20 +38,41 @@ $this->params['breadcrumbs'][] = $this->title;
                 'template' => '{change-status} {remove-process} {view}',
                 'buttons' => [
                     'change-status' => function ($url, $model) {
-                        if($model->status == 'draft')
-                            return Html::a('<i class="glyphicon glyphicon-ok" style="color:green;"></i>', ['vendor-liquidation/change-status-liquidation', 'id' => $model->vendor_liquidation_process_id], ['data-pjax' => '0']);
+                        if ($model->status == 'draft')
+                            return Html::a(
+                                '<i class="glyphicon glyphicon-ok" style="color:green;"></i>',
+                                [
+                                    'vendor-liquidation/change-status-liquidation',
+                                    'id' => $model->vendor_liquidation_process_id
+                                ],
+                                ['data-pjax' => '0']
+                            );
                     },
                     'remove-process' => function ($url, $model) {
-                        if($model->status == 'draft')
-                            return Html::a('<i class="glyphicon glyphicon-remove" style="color:red;"></i>', ['vendor-liquidation/remove-vendor-liquidation-process', 'id' => $model->vendor_liquidation_process_id], ['data-pjax' => '0']);
+                        if ($model->status == 'draft')
+                            return Html::a(
+                                '<i class="glyphicon glyphicon-remove" style="color:red;"></i>',
+                                [
+                                    'vendor-liquidation/remove-vendor-liquidation-process',
+                                    'id' => $model->vendor_liquidation_process_id
+                                ],
+                                ['data-pjax' => '0']
+                            );
                     },
                     'view' => function ($url, $model) {
-                        if($model->status == 'pending')
-                            return Html::a('<i class="glyphicon glyphicon-eye-open" ></i>', ['vendor-liquidation/view-vendor-liquidation-process', 'id' => $model->vendor_liquidation_process_id], ['data-pjax' => '0']);
+                        if ($model->status == 'pending')
+                            return Html::a(
+                                '<i class="glyphicon glyphicon-eye-open" ></i>',
+                                [
+                                    'vendor-liquidation/view-vendor-liquidation-process',
+                                    'id' => $model->vendor_liquidation_process_id
+                                ],
+                                ['data-pjax' => '0']
+                            );
                     }
                 ]
             ]
         ]
     ]); ?>
-    
+
 </div>
