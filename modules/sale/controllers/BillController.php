07555c0e57e156bf429f03cd540c4ce912112424
dexterlab10 <dexterlab10@protonmail.com>
2022-03-28 11:17:25 -0300

    mini FIX: little incompatibility error between php versions. No functionallity affected.

--- modules/sale/controllers/BillController.php
+++ modules/sale/controllers/BillController.php
@@ -111,16 +111,16 @@ class BillController extends Controller
 
             $graphDataProvider = $searchModel->searchHistory(Yii::$app->request->getQueryParams());
             $graphDataProvider->sort = false;
-
-            $first = array_shift($graphDataProvider->getModels());
-            $last = array_pop($graphDataProvider->getModels());
+            $graphModels = $graphDataProvider->getModels();
+            $first = array_shift($graphModels);
+            $last = array_pop($graphModels);
 
             if ($first != null and $last != null) {
                 $graphData->fromdate = $first->date;
                 $graphData->todate = $last->date;
             }
 
-            $graphData->steps = \yii\helpers\ArrayHelper::getColumn($graphDataProvider->getModels(), 'date');
+            $graphData->steps = \yii\helpers\ArrayHelper::getColumn($graphModels, 'date');
 
             //Datos
             $graphData->dataProvider = $graphDataProvider;
