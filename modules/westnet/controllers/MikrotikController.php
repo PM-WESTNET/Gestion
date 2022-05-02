3e11028fa63d91f32d85e07392c8549fc60b2824
emilasheras <emperorbfd@gmail.com>
2022-05-02 17:30:59 -0300

    WIP: advances with mikrotik controller for soldef interfacing. Now clients connected to Clientes Fibra server should be able to be updated.

--- modules/westnet/controllers/MikrotikController.php
+++ modules/westnet/controllers/MikrotikController.php
@@ -16,6 +16,7 @@ use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use yii\db\Expression;
 use yii\db\Query;
 use app\modules\westnet\models\Node;
+use PDO;
 
 /**
  * AccessPointController implements the CRUD actions for AccessPoint model.
@@ -28,39 +29,51 @@ class MikrotikController extends Controller
      */
     public static function updateQueues($connection, $old_ip4_1 = null, $old_node_id = null)
     {
-        // return false if no server is associated OR of it isnt a mikrotik type server connection
-        if (!isset($connection->server,$connection->server->load_balancer_type,$connection->server->ip_of_load_balancer) or 
-        !($connection->server->load_balancer_type == 'Mikrotik')) return false;
-        $mikrotikIP = long2ip($connection->server->ip_of_load_balancer);
-        $responseInfo = false; // defaults as false
+        // if it isnt a mikrotik type server connection or is disabled
+        if (!($connection->server->load_balancer_type == 'Mikrotik') || ($connection->server->status != 'enabled')) return false;
+        // return false if no server is associated
+        if (!isset($connection->server,$connection->server->load_balancer_type)) return false;
 
-        // create queue on mikrotik server
-        $queueCreated = self::createMikrotikQueue($connection, $mikrotikIP);
-        if (is_string($queueCreated) && isset(Yii::$app->session)) Yii::$app->session->addFlash('info', $queueCreated.' on server: '.$connection->server->name);
-        
+        // if the mikrotik server has a known IP for GestionApp then it is most probably a wireless connection to be updated (without soldef help)
+        if (!empty($connection->server->ip_of_load_balancer)){
+            $mikrotikIP = long2ip($connection->server->ip_of_load_balancer);
+
+            // create queue on mikrotik server
+            $queueCreated = self::createMikrotikQueue($connection, $mikrotikIP);
+            
+            // delete queue in mikrotik server
+            if (!is_null($old_ip4_1)) {
+                $mikrotikIPDelete = $mikrotikIP;
+    
+                // delete the queue of the server where it was left off last time (could be a different mikrotik server)
+                if (!is_null($old_node_id)) {
+                    // var_dump('$old_node_id',$old_node_id);
+                    $oldNode = Node::findOne($old_node_id); // in case the node changed, we search for it and its server's IP number
+                    $mikrotikIPDelete = long2ip($oldNode->server->ip_of_load_balancer);
+                    // var_dump('$mikrotikIPDelete',$mikrotikIPDelete);
+                }
+    
+                $queueDeleted = self::deleteMikrotikQueue($mikrotikIPDelete,$old_ip4_1);
+                if (is_string($queueDeleted) && isset(Yii::$app->session)) Yii::$app->session->addFlash('info', $queueDeleted.' on server: '.$connection->server->name);
+                if (is_bool($queueDeleted) && isset(Yii::$app->session)) Yii::$app->session->addFlash('error', 'Failed to delete Queue on Mikrotik server'.$connection->server->name);
+            }
+        } 
+        // if IS mikrotik but doesnt have an IP associated, Soldef controls it (FTTH logic)
+        else 
+        {
+            // check if the current connection has an FTTH plan associated, then run Soldef api
+            if(!empty(self::getPlanTechnologyCategory($connection))){
+                $queueCreated = self::updateMikrotikQueueFTTH($connection, $old_ip4_1);
+            }
+        }
+
         // if FALSE
         if (is_bool($queueCreated) && !$queueCreated) {
             if(isset(Yii::$app->session)) Yii::$app->session->addFlash('error', 'Failed to create Queue on Mikrotik server'.$connection->server->name);
             return false;
         }
-        
-        // delete queue in mikrotik server
-        if (!is_null($old_ip4_1)) {
-            $mikrotikIPDelete = $mikrotikIP;
-
-            // delete the queue of the server where it was left off last time (could be a different mikrotik server)
-            if (!is_null($old_node_id)) {
-                // var_dump('$old_node_id',$old_node_id);
-                $oldNode = Node::findOne($old_node_id); // in case the node changed, we search for it and its server's IP number
-                $mikrotikIPDelete = long2ip($oldNode->server->ip_of_load_balancer);
-                // var_dump('$mikrotikIPDelete',$mikrotikIPDelete);
-            }
-
-            $queueDeleted = self::deleteMikrotikQueue($connection,$mikrotikIPDelete,$old_ip4_1);
-            if (is_string($queueDeleted) && isset(Yii::$app->session)) Yii::$app->session->addFlash('info', $queueDeleted.' on server: '.$connection->server->name);
-            if (is_bool($queueDeleted) && isset(Yii::$app->session)) Yii::$app->session->addFlash('error', 'Failed to delete Queue on Mikrotik server'.$connection->server->name);
-        }
-
+        else if (is_string($queueCreated) && isset(Yii::$app->session)) Yii::$app->session->addFlash('info', $queueCreated.' on server: '.$connection->server->name);
+            
         return $queueCreated;
     }
 
@@ -108,7 +121,7 @@ class MikrotikController extends Controller
     /**
      * deletes a queue on a Mikrotik server based on its IP and the current connections data.
      */
-    private function deleteMikrotikQueue($connection,$mikrotikIP,$old_ip4_1)
+    private function deleteMikrotikQueue($mikrotikIP,$old_ip4_1)
     {
         $dataDel = array(
             "ip" => $mikrotikIP, // mikrotik ip
@@ -208,4 +221,82 @@ class MikrotikController extends Controller
 
         return $responseString; //string or false
     }
+
+    /**
+     * creates a mikrotik queue with the help of SOLDEF and RADIUS.
+     * used for FTTH connections that pass through mikrotik load balancers
+     * returns false if theres a failure
+     */
+    private function updateMikrotikQueueFTTH($connection,$old_ip4_1 = null, $http_method = 'POST'){
+        $base_url = $connection->server->url; // gives something like https://172.27.2.31/ or https://soldef.westnet.com.ar/
+        $base_url = 'https://soldef.westnet.com.ar/';
+        $endpoint = 'api/v1/fibra/Update_Radius_Plan_API';
+        $api_url = $base_url.$endpoint;
+        $current_ip4_1 = long2ip($connection->ip4_1); // convert to ip. if null or 0 => 0.0.0.0
+        $old_ip4_1 = is_null($old_ip4_1) ? $current_ip4_1 : $old_ip4_1; // if there is no old_ip, use current_ip value
+
+        // var_dump($base_url);
+        // var_dump($api_url);
+        // var_dump($current_ip4_1);
+
+        $queue_data = array(
+            "ip_anterior" => $old_ip4_1, // old ip
+            "ip_nueva" => $current_ip4_1 //connection->ip
+        );
+        $data = json_encode($queue_data); //json conv
+        // var_dump($data);
+
+        // curl setup
+        $conexion = curl_init();
+        curl_setopt($conexion, CURLOPT_URL, $api_url);
+        curl_setopt($conexion, CURLOPT_POSTFIELDS, $data);
+        curl_setopt(
+            $conexion,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json'
+            )
+        );
+        curl_setopt($conexion, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($conexion, CURLOPT_CUSTOMREQUEST, $http_method);
+        $respuesta = '';
+        $respuesta = curl_exec($conexion);
+        $HTTP_CODE_RESPONSE = curl_getinfo($conexion, CURLINFO_HTTP_CODE); // get http response code : 200,401,500...
+        // var_dump(curl_error($conexion));
+        // var_dump($respuesta);
+        curl_close($conexion); // connection close
+        // format response
+        if (is_array($respuesta)) $respuesta = implode('. ', $respuesta); // if is array, transform into string for response
+        $responseString = ($respuesta . ' HTTP_CODE: ' . $HTTP_CODE_RESPONSE);
+
+        return $responseString; //string or false
+    }
+
+    /**
+     * gets the current connection's contract's plan technology category. 
+     * could be wifi or fiber optics but is only used for fiber mikrotik connections.
+     * 
+     */
+    private function getPlanTechnologyCategory($connection)
+    {
+        // select , from , join
+        $query = (new Query())
+            ->select(['*'])
+            ->from('connection conn')
+            ->leftJoin('contract cont', 'conn.contract_id = cont.contract_id')
+            ->leftJoin('contract_detail cd', 'cd.contract_id = cont.contract_id')
+            ->leftJoin('product prod', 'cd.product_id = prod.product_id')
+            ->leftJoin('product_has_category phcat', 'phcat.product_id = prod.product_id')
+            ->leftJoin('category cat', 'phcat.category_id = cat.category_id');
+
+        //where
+        $query->andWhere(['prod.type' => 'plan']);
+        $query->andWhere(['cat.category_id' => '16']); // todo: change this hardcoded ID for FTTH plans . FUCK.
+        $query->andWhere(['conn.connection_id' => $connection->connection_id]);
+
+        //exec
+        $qResults = $query->one();
+        return $qResults;
+    }
+
 }
