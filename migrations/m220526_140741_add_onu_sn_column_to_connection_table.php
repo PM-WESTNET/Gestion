d7737f338cac0814ed958d18b4b6890e2a88372f
samyfontes <samuelfontes.coder@gmail.com>
2022-05-26 12:15:18 -0300

    agrega migracion de columna onu_sn a tabla connection

---
+++ migrations/m220526_140741_add_onu_sn_column_to_connection_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding onu_sn to table `connection`.
+ */
+class m220526_140741_add_onu_sn_column_to_connection_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('connection', 'onu_sn', $this->varchar(50));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('connection', 'onu_sn');
+    }
+}
