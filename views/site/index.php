e9c9beeaf2551e00a9d5fefd6b7982372af874b9
emilasheras <emperorbfd@gmail.com>
2022-06-22 09:55:44 -0300

    added size cap for company logo on site-index. Also inverted where alerts are shown on the index page

--- views/site/index.php
+++ views/site/index.php
@@ -10,18 +10,9 @@ use yii\web\User;
 $this->title = Yii::$app->params['web_title'];
 ?>
 
-<?php if(isset($payment_intention_accountability)): ?>
-        <div>
-            <a href=<?= Url::toRoute(['westnet/notifications/siro/checker-of-payments'])?> >
-                <h2>
-                    <span class="label label-danger">Hay <?= count($payment_intention_accountability) ?> intenciones de pagos sin contrastar.</span>
-                </h2>
-            </a>
-        </div>
-    <?php endif; ?>
 <div class="jumbotron">
     <?php if (!empty(Yii::$app->params['web_logo'])): ?>
-        <img alt="<?= Yii::$app->params['web_title']; ?>" src="<?='/'.Yii::$app->params['path'].'/'.Yii::$app->params['web_logo']?>"/>
+        <img class="company-logo" alt="<?= Yii::$app->params['web_title']; ?>" src="<?='/'.Yii::$app->params['path'].'/'.Yii::$app->params['web_logo']?>"/>
 
     <?php endif; ?>
 
@@ -30,3 +21,12 @@ $this->title = Yii::$app->params['web_title'];
 
 <hr/>
 
+<?php if(isset($payment_intention_accountability)): ?>
+        <div>
+            <a href=<?= Url::toRoute(['westnet/notifications/siro/checker-of-payments'])?> >
+                <h2>
+                    <span class="label label-danger">Hay <?= count($payment_intention_accountability) ?> intenciones de pagos sin contrastar.</span>
+                </h2>
+            </a>
+        </div>
+<?php endif; ?>
